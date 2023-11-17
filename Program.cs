254058421ac647df006a914eb2b09794253426f1
Ádám Podlipnik <podlipnik.adam@gmail.com>
2023-11-17 14:54:57 +0100

    number class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ikt {
+    public class Program {
+        static void Main() {
+            Number a = new(numberGen());
+            a.Show();
+            int numberGen() {
+                Random rnd = new();
+                return rnd.Next(1, 3)*2;
+            }
+        }
+        
+    }
+
+
+
+
+    public class Number {
+        public int value;
+        private ConsoleColor color;
+        public Number(int Value) {
+            value = Value;
+            if (value <= 4)
+                color = ConsoleColor.White;
+            else if (value >= 8 && value <= 64)
+                color = ConsoleColor.Red;
+            else
+                color = ConsoleColor.Yellow;
+        }
+        public void Show() {
+            Console.ForegroundColor = color;
+            Console.Write(value);
+            Console.ResetColor();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# ikt_2048
+# IKT PROJEKTMUNKA
+**Készítették: Podlipnik Ádám és Zsoldos Donát Dominik**
+> [!NOTE]
+> Ha két egyenlõ szám találkozik, egy új mezõben összeadódnak.
+> A játék lényege egyszerû: Juss el 2048-ig!
