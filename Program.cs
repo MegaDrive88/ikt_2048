a0ca731b8df3fbcfadfcbe7b874108e8ea211671
Ádám Podlipnik <podlipnik.adam@gmail.com>
2023-12-05 18:34:06 +0100

    some refactors

--- Program.cs
+++ Program.cs
@@ -8,10 +8,23 @@
                 Game.Move();
             }            
         }
-    } 
+    }
+    public static class StartingNums {
+        public static int[] Get(ConsoleKey k) {
+            return k switch {
+                ConsoleKey.UpArrow => new[] { 1, 0, -1, 0, 0, 1, 1, -3 },
+                ConsoleKey.DownArrow => new[] { 2, 0, 1, 0, 0, -1, 1, 3 },
+                ConsoleKey.LeftArrow => new[] { 0, 1, 0, -1, 1, 0, -3, 1 },
+                ConsoleKey.RightArrow => new[] { 0, 2, 0, 1, -1, 0, 3, 1 },
+                ConsoleKey.Z => new[] { -1, -1, 0 },
+                ConsoleKey.Escape => new[] { -1, -1, 1 },
+                _ => new[] { -1, -1, -1 },
+            };
+        }
+    }
     public static class Game {
-        static Random rnd = new();
-        static Tile[,] table = new Tile[4, 4];
+        private static Random rnd = new();
+        private static Tile[,] table = new Tile[4, 4];
         static int score = 0;
         public static bool Ready() {
             Console.CursorVisible = false;
@@ -28,9 +41,9 @@
             }
         }
         public static void Starting() {
-            for (int i = 0; i < table.GetLength(0); i++) {
+            for (int i = 0; i < 4; i++) {
                 for (int j = 0; j < 4; j++) {
-                    table[i, j] = new Tile(0);
+                    table[i, j] = new(0);
                 }
             }
             numberGen();
@@ -39,68 +52,53 @@
         }
         public static void Move() {
             //fileWrite(prev);
-            Transpose(GetStartingNums(Console.ReadKey(true).Key));
-            //nyert e? -- nem kell kulon fuggveny
-            numberGen();
-            tablePrint();
+            Transpose(StartingNums.Get(Console.ReadKey(true).Key));
+            //nyert e? -- nem kell kulon fuggveny  
         }
         public static bool Over() {
             return false;
         }
-        private static object[] GetStartingNums(ConsoleKey k) {
-            Dictionary<ConsoleKey, object[]> nums = new Dictionary<ConsoleKey, object[]>
-            {
-                { ConsoleKey.UpArrow, new object[] { 1, 0, -1, 0, 0, 1, 1, -3 } }, // ezt vhogy refactorolni kéne
-                { ConsoleKey.DownArrow, new object[] { 2, 0, 1, 0, 0, -1, 1, 3 } },// de legalább műxik
-                { ConsoleKey.LeftArrow, new object[] { 0, 1, 0, -1, 1, 0, -3, 1 } },
-                { ConsoleKey.RightArrow, new object[] { 0, 2, 0, 1, -1, 0, 3, 1 } },
-                { ConsoleKey.Z, new object[] { -1, -1, "Undo" } },
-                { ConsoleKey.Escape, new object[] { -1, -1, "Exit" } },
-            };
-            try {
-                return nums[k];
-            }
-            catch {
-                return new object[] { -1, -1, "Invalid" };
-            }
-        }
-        private static void Transpose(object[] nums) {
-            int y = (int)nums[0], x = (int)nums[1];
+        private static void Transpose(int[] nums) {
+            int y = nums[0], x = nums[1];
             bool moved = false;
             if (y != -1) {
-                int scout_y = y + (int)nums[2], scout_x = x + (int)nums[3];
+                int scout_y = y + nums[2], scout_x = x + nums[3];
                 for (int i = 0; i < 4; i++) {
                     for (int j = 0; j < 3; j++) {
                         try {
                             while (table[scout_y, scout_x].isEmpty) {
                                 moved = true;
-                                scout_y += (int)nums[2];
-                                scout_x += (int)nums[3];
+                                scout_y += nums[2];
+                                scout_x += nums[3];
                                 //valahova ide kéne majd az összeadás
                             }
                         }
                         catch { }
-                        scout_y -= (int)nums[2];
-                        scout_x -= (int)nums[3];
+                        scout_y -= nums[2];
+                        scout_x -= nums[3];
                         table[scout_y, scout_x] = table[y, x];
                         table[y, x] = moved ? new(0) : table[y, x];
-                        x += (int)nums[4];
-                        y += (int)nums[5];
-                        scout_y = y + (int)nums[2];
-                        scout_x = x + (int)nums[3];
+                        x += nums[4];
+                        y += nums[5];
+                        scout_y = y + nums[2];
+                        scout_x = x + nums[3];
                         moved = false;
                     }
-                    x += (int)nums[6];
-                    y += (int)nums[7];
-                    scout_y = y + (int)nums[2];
-                    scout_x = x + (int)nums[3];
+                    x += nums[6];
+                    y += nums[7];
+                    scout_y = y + nums[2];
+                    scout_x = x + nums[3];
                 }
+                numberGen(); // csak ha volt mozgás
+                tablePrint();
             }
             else {
                 switch (nums[2]) {
-                    case "Undo":
+                    case 0:
+                        //undo
                         break;
-                    case "Exit":
+                    case 1:
+                        // exit
                         break;
                     default:
                         break;
@@ -133,14 +131,14 @@
         }
     }
     public class Tile {
-        public int value { get; private set; }
+        public int Value { get; private set; }
         private ConsoleColor color;
-        public bool isEmpty { get { return value == 0; } }
-        public Tile(int Value) {
-            value = Value;
-            if (value <= 4)
+        public bool isEmpty { get { return Value == 0; } }
+        public Tile(int value) {
+            Value = value;
+            if (Value <= 4)
                 color = ConsoleColor.White;
-            else if (value >= 8 && value <= 64)
+            else if (Value >= 8 && Value <= 64)
                 color = ConsoleColor.DarkRed;
             else
                 color = ConsoleColor.DarkYellow;
@@ -149,7 +147,7 @@
             if (!isEmpty) {
                 Console.BackgroundColor = color;
                 Console.ForegroundColor = ConsoleColor.Black;
-                Console.Write($"[{new string(' ', (6 - value.ToString().Length) / 2)}{value}{new string(' ', (5 - value.ToString().Length) / 2)}]");
+                Console.Write($"[{new string(' ', (6 - Value.ToString().Length) / 2)}{Value}{new string(' ', (5 - Value.ToString().Length) / 2)}]");
                 Console.BackgroundColor = ConsoleColor.Black;
             }
             else {
@@ -159,7 +157,7 @@
             }
         }
         public static Tile operator+(Tile a, Tile b) {
-            return new(a.value + b.value);
+            return new(a.Value + b.Value);
         }
     }
 }
